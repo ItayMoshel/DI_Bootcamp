518cb78ca6ef31cbf0e54ac1c9bec6188e1c1b2c
איתי מושל <itaym@h-MacBook-Pro-sl-yty.local>
2021-09-12 23:29:10 +0300

    added exercises

---
+++ Week7/Day2/ninja.py
@@ -0,0 +1,19 @@
+def somthing():
+    string = input("input a string\n")
+    char = input("input a character\n")
+    num_of_char = string.count(char)
+    print(num_of_char)
+
+
+somthing()
+
+s = input("String: ")
+ch = input("Character: ")
+
+
+def hey():
+    num_of_char = s.count(ch)
+    print(num_of_char)
+
+
+hey()
---
+++ Week7/Day4/ninja.py
@@ -0,0 +1,12 @@
+# def get_full_name(first_name, last_name, middle_name=None):
+#     print(first_name, middle_name, last_name)
+#
+#
+# get_full_name("jakob", "ibrahim", "sisi")
+
+
+
+# def get_full_name(first_name, last_name, *middle_name):
+#     print(first_name, middle_name, last_name,)
+#
+# get_full_name("jacob", "ibrahim")
