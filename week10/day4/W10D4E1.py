d3466cfc6a3a344b1408636b7d61ab5f102e337c
איתי מושל <itaym@h-MacBook-Pro-sl-yty.local>
2021-09-30 01:43:21 +0300

    Week10Day4Exercise1

---
+++ week10/day4/W10D4E1.py
@@ -0,0 +1,27 @@
+import random
+
+with open('file.txt', 'r') as words:
+    def get_words_from_file():
+        return words.read().splitlines()
+
+
+    def get_random_sentence(length):
+        words_list = get_words_from_file()
+        new_list = []
+        for i in range(0, length):
+            new_list.append(random.choice(words_list))
+        print(" ".join(new_list))
+
+
+    def main():
+        print("This program takes random lines from another file and concatenates them into a sentence.")
+        length = int(input("How long would you like the sentence will be between 2 and 20?\n"))
+        if 2 <= length <= 20:
+            get_random_sentence(length)
+        else:
+            print("Program ended.")
+        # except ValueError:
+        #     print("Ending program.")
+
+
+    main()
