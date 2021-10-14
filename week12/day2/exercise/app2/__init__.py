359adf2b42d255aa623a12e960d1f36eb69ba5c9
איתי מושל <itaym@h-MacBook-Pro-sl-yty.local>
2021-10-14 11:45:54 +0300

    week12day2exercise

--- week12/day2/exercise/app2/__init__.py
+++ week12/day2/exercise/app2/__init__.py
@@ -6,5 +6,7 @@ from app2 import testkey
 app = flask.Flask(__name__)
 app.config.from_object(testkey.Config)
 
+list_of_cities = []
+
 import app2.routes
 import app2.form
--- week12/day2/exercise/app2/routes.py
+++ week12/day2/exercise/app2/routes.py
@@ -1,36 +1,39 @@
-from flask import Flask, render_template
-
+from flask import render_template
 import flask
-
-from app2 import app
+from app2 import app, list_of_cities
 from app2.form import Form
-
-
-@app.route("/")
-def home():
-    return render_template("base.html")
 
 
 @app.route('/add-city', methods=("GET", "POST"))
 def add_city():
     form = Form()
-    if form.validate_on_submit():  # Check if the form has been filled
+    if form.validate_on_submit():
         cityname = form.cityname.data
         citycountry = form.citycountry.data
         numofinhabitants = form.numofinhabitats.data
         cityarea = form.cityarea.data
-
-        print("Here is what I got from the form:")
-        print("City's name: ", cityname)
-        print("City's country: ", citycountry)
-        print("City's area : ", cityarea)
-        print("Number of inhabitants : ", numofinhabitants)
+        capital = form.is_capital
+        dict = {
+            "city name": cityname,
+            "city country": citycountry,
+            "city area": cityarea,
+            "number of inhabitants": numofinhabitants,
+            "capital": capital,
+        }
+        list_of_cities.append(dict)
 
         return flask.redirect('/')
     return render_template("add_city.html", form=form)
+
+
+@app.route("/")
+def home():
+    return f"{list_of_cities}"
 
 
 @app.errorhandler(404)
 def page_not_found(error):
     print(error)
     return render_template("404.html"), 404
+
+
